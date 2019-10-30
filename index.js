069b393740eaf246cb141bf80830f36943ce2a07
xujiujiu <906784584@qq.com>
2019-10-30 15:32:28 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const getPath = require('./lib/index')
+const fs = require('fs')
+const path = require('path')
+
+
+
+program.version(require('./package').version)
+        .usage('[options] <filename ...>')
+        .option('-f, --filename <filename>', '路由文件, 默认src/router/router.js')
+        .action(({filename = '/src/router/router.js'}) => {
+            let data = getPath(filename)
+            !data.error && console.log('finish!')
+        })
+        .parse(process.argv)
+
+module.exports = {
+    getPath
+}
